fbf5931e6c3f04a76597628369f5c0387b0a598d
Salvador Olvera <>
2021-04-17 23:49:52 +0000

    fix: constructorPattern

--- Patterns/ConstructorPattern.js
+++ Patterns/ConstructorPattern.js
@@ -2,15 +2,14 @@ function Car (name, color, isAutomatic) {
   this.name = name;
   this.color = color;
   this.isAutomatic = isAutomatic || false;
-  
 
   this.transmissionType = function() {
-    return"This car is Automatic" + "This car is manual";
+    console.log(this.isAutomatic ? "This car is Automatic" : "This car is manual");
   }
 }
-
 
 const car1 = new Car("SX", "red", true);
 const car2 = new Car("LX", "blue");
 
-console.log(car1.transmissionType)
+car1.transmissionType();
+car2.transmissionType();
